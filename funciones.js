6ef34915adb011434ae9bb7ab6ebdf8fe57b95d8
mariap99 <maria99merlo@gmail.com>
2019-06-24 23:46:43 -0300

    se realizaron correcciones

--- funciones.js
+++ funciones.js
@@ -1,14 +1,37 @@
+/** <form></form>
+ * Calculo de la hipotenusa<form></form>
+ * @method operacion<form></form>
+ * @param Cateto1<form></form>
+ * @param Cateto2<form></form>
+ * @return Hipotenusa<form></form>
+ * Calculo del area<form></form>
+ * @method area<form></form>
+ * @param Cateto1<form></form>
+ * @param Cateto2<form></form>
+ * @return area<form></form>
+ * Calculo del perimetro<form></form>
+ * @method perimetro<form></form>
+ * @param Cateto1<form></form>
+ * @param Cateto2<form></form>
+ * @param Hipotenusa<form></form>
+ * @return perimetro<form></form>
+ **/
+
 function operacion() {
 
     var catA = document.pitagoras.Cateto1.value;
     var catB = document.pitagoras.Cateto2.value;
 
+    if(catA>0 || catB>0){
 
         var a = Math.pow(catA, 2);
         var b = Math.pow(catB, 2);
         var c = Math.sqrt(a+b);
 
         document.pitagoras.Hipotenusa.value=c.toFixed(2);
+    }
+
+    else{ alert("DEBE INGRESAR VALORES POSITIVOS"); }
 
     }
 
